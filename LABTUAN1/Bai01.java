27f2a00a9487b28f656883ef28b8bd3c9efd8637
Pham Ngoc Khoi <g2ng0ckh0i@gmail.com>
2025-04-01 13:42:54 +0700

    Add files via upload

---
+++ LABTUAN1/Bai01.java
@@ -0,0 +1,10 @@
+public class Bai01 {
+    public static void main(String[] args) {
+        String hoTen = "Phạm Ngọc Khôi";
+        String ngaySinh = "09/06/2005";
+        String mssv = "1250080086";
+        System.out.println("Họ tên: " + hoTen);
+        System.out.println("Ngày sinh: " + ngaySinh);
+        System.out.println("Mã số sinh viên: " + mssv);
+    }
+}
